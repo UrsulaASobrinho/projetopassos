3646da97b2a83d5d69d48fea81c99fcad8a1cdce
Úrsula Sobrinho <52325174+UrsulaASobrinho@users.noreply.github.com>
2020-01-26 08:10:02 -0300

    configuraçoes

--- scr/index.js
+++ scr/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 //settingss
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
@@ -13,6 +13,7 @@ app.set('view engine', 'ejs');
 app.use(require('./routes/index'));
 //start files
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(app.get('port'), () => {
     console.log('servidor on port', app.get('port'));
 });
